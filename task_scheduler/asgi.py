b26c92ab208ed5d7554d9fa283dce655f023004c
Princeton Afeez <princetonafeez@gmail.com>
2026-06-24 08:46:58 -0700

    ASGI for the scheduler app

---
+++ task_scheduler/asgi.py
@@ -0,0 +1,10 @@
+""" ASGI for the scheduler app. """
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_scheduler.settings")
+
+application = get_asgi_application()
+
