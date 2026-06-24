6c7a1651eb52ee856553395e8825ddafca752207
Princeton Afeez <princetonafeez@gmail.com>
2026-06-24 08:48:09 -0700

    Test settings for the scheduler app

---
+++ task_scheduler/test_settings.py
@@ -0,0 +1,22 @@
+""" Test settings for the scheduler app. """
+
+from .settings import *  # noqa: F401,F403
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "task-scheduler-tests",
+    }
+}
+
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+WEBUI_AUTH_ENABLED = True
+
