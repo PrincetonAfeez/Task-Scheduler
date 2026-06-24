ec0799ccbed12018bf9dbb809c8395f8693836d5
Princeton Afeez <princetonafeez@gmail.com>
2026-06-24 08:48:43 -0700

    WSGI for the scheduler app

---
+++ task_scheduler/wsgi.py
@@ -0,0 +1,10 @@
+""" WSGI for the scheduler app. """
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_scheduler.settings")
+
+application = get_wsgi_application()
+
